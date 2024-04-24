cd986f6c06989302840c1d44d71e084957e2bd62
Farmaan16 <farmanmuzammil17gmail.com>
2024-04-24 14:42:17 +0530

    "added loader on logout button"

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,13 +4,14 @@ import axios from "axios";
 import Link from "next/link";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import Spinner from "@/components/Spinner";
 
 
 export default function Profilepage() {
     const router = useRouter()
   const [userData, setUserData] = useState<{ _id: string } | null>(null);
   const [showButton, setShowButton] = useState(true);
-
+const [loading, setLoading] = useState(false);
     const getUserDetails = async () => { 
         try {
           const response = await axios.post("/api/users/me");
@@ -20,20 +21,23 @@ export default function Profilepage() {
         } catch (error: any) {
             console.log(error.message)
             toast.error(error.message)
-        }
+        } 
     }
 
-    const onLogout = async () => {
-        try {
-            const response = await axios.get("/api/users/logout")
-            toast.success("logged out successfully")
-            console.log(response.data)
-            router.push("/login")
-        } catch (error: any) {
-            console.log(error.message)
-            toast.error(error.message)
-        }
+  const onLogout = async () => {
+    setLoading(true); // Set loading to true when logout button is clicked
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success("logged out successfully");
+      console.log(response.data);
+      router.push("/");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false); // Set loading back to false after logout operation completes
     }
+  }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
@@ -73,11 +77,13 @@ export default function Profilepage() {
         )}
       </div>
 
+      {/* Render the logout button with spinner */}
       <button
         onClick={onLogout}
-        className="  mt-6 bg-black dark:bg-inherit rounded-full w-fit text-white dark:text-neutral-300 px-4 py-2 font-bold border border-neutral-600 hover:bg-red-900"
+        className="  mt-6 bg-black dark:bg-inherit rounded-full w-fit text-white dark:text-neutral-300 px-4 py-2 font-bold border border-neutral-600 hover:bg-red-900 relative"
+        disabled={loading} // Disable the logout button when loading is true
       >
-        Logout
+       {loading ? <Spinner /> : "Logout"}
       </button>
     </div>
   );
